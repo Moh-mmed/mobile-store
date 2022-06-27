a616a51c409138d50228d6e45d84b42a43f654e7
mohammedBnomr <moohaboy@gmail.com>
2022-06-27 12:58:46 +0300

    convert html to php

---
+++ footer.php
@@ -0,0 +1,68 @@
+</main>
+<!-- !end #main-site -->
+
+<!-- start #footer -->
+<footer id="footer" class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-12">
+                <h4 class="font-size-20">Mobile Store</h4>
+                <p class="font-size-14 text-white-50">Lorem ipsum dolor sit amet consectetur, adipisicing
+                    elit. Repellendus, deserunt.</p>
+            </div>
+            <div class="col-lg-4 col-12">
+                <h4 class=" font-size-20">Newslatter</h4>
+                <form class="form-row">
+                    <div class="col d-inline-block mb-2">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="col d-inline-block mb-2">
+                        <button type="submit" class="btn btn-primary">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-2 col-12">
+                <h4 class=" font-size-20">Information</h4>
+                <div class="d-flex flex-column flex-wrap">
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">About Us</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Delivery Information</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Privacy Policy</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Terms & Conditions</a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-12">
+                <h4 class=" font-size-20">Account</h4>
+                <div class="d-flex flex-column flex-wrap">
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">My Account</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Order History</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Wish List</a>
+                    <a href="#" class=" font-size-14 text-white-50 pb-1">Newslatters</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="copyright text-center bg-dark text-white py-2">
+    <p class=" font-size-14">&copy; Copyrights 2020. Desing By <a href="#" class="color-second">benaoucode</a></p>
+</div>
+<!-- !end #footer -->
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!-- Owl Carousel JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" integrity="sha512-gY25nC63ddE0LcLPhxUJGFxa2GoIyA5FLym4UJqHDEMHjp8RET6Zn/SHo1sltt3WuVtqfyxECP38/daUc/WVEA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Isotope JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha512-Zq2BOxyhvnRFXu0+WE6ojpZLOU2jdnqbrM1hmVdGzyeCa1DgM3X5Q4A/Is9xA1IkbUeDd7755dNNI/PzSf2Pew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Custom Javascript -->
+<script src="index.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Style -->
+    <link rel="stylesheet" href="./style.css">
+    <title>Mobile Store</title>
+</head>
+
+<body>
+
+    <!-- start #header -->
+    <header id="header">
+        <div class="strip d-flex justify-content-between px-4 py-1 bg-light">
+            <p class=" font-size-12 text-black-50 m-0">Aymen Jade St. Duluth Washington 92611
+                (222) 222-2222</p>
+            <div class=" font-size-14">
+                <a href="#" class="px-3 border-right border-left text-dark">Login</a>
+                <a href="#" class="px-3 border-right text-dark">Wishlist (0)</a>
+            </div>
+        </div>
+
+        <!-- Primary Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light color-second-bg">
+            <a class="navbar-brand" href="#">Mobile Store</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto ">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">On Sale</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Category</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products <i class="fas fa-chevron-down"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Category <i class="fas fa-chevron-down"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Coming Soon</a>
+                    </li>
+                </ul>
+                <form action="#" class="font-size-14">
+                    <a href="#" class="py-2 rounded-pill color-primary-bg">
+                        <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
+                        <span class="px-3 py-2 rounded-pill text-dark bg-light">0</span>
+                    </a>
+                </form>
+            </div>
+        </nav>
+
+        <!-- !Primary Navigation -->
+
+    </header>
+    <!-- !end #header -->
+
+    <!-- start #main-site -->
+    <main id="main-site">
